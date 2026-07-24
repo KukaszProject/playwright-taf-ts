f80f8a41322e7bdf86ae58e4159ec6b839b066f6
Lukasz Kielbasa <hamsingming@gmail.com>
2026-07-24 11:51:17 +0200

    Removed unnecessary lines from fixture

--- fixtures/base.test.ts
+++ fixtures/base.test.ts
@@ -1,38 +1,34 @@
-import {test as baseTest} from '@playwright/test';
-import {LoginPage} from '../pages/login.page';
-import {InventoryPage} from '../pages/inventory.page';
-import {CartPage} from '../pages/cart.page';
-import {CheckoutPage} from '../pages/checkout.page';
-import {HeaderComponent} from '../components/header.component';
+import { test as baseTest } from '@playwright/test';
+import { LoginPage } from '../pages/login.page';
+import { InventoryPage } from '../pages/inventory.page';
+import { CartPage } from '../pages/cart.page';
+import { CheckoutPage } from '../pages/checkout.page';
+import { HeaderComponent } from '../components/header.component';
 
 type TestFixtures = {
-    loginPage: LoginPage;
-    inventoryPage: InventoryPage;
-    cartPage: CartPage;
-    checkoutPage: CheckoutPage;
-    headerComponent: HeaderComponent;
+  loginPage: LoginPage;
+  inventoryPage: InventoryPage;
+  cartPage: CartPage;
+  checkoutPage: CheckoutPage;
+  headerComponent: HeaderComponent;
 };
 
 export const test = baseTest.extend<TestFixtures>({
-    page: async ({page}, use) => {
-        await use(page);
-        await page.close();
-    },
-    loginPage: async ({page}, use) => {
-        await use(new LoginPage(page));
-    },
-    inventoryPage: async ({page}, use) => {
-        await use(new InventoryPage(page));
-    },
-    cartPage: async ({page}, use) => {
-        await use(new CartPage(page));
-    },
-    checkoutPage: async ({page}, use) => {
-        await use(new CheckoutPage(page));
-    },
-    headerComponent: async ({page}, use) => {
-        await use(new HeaderComponent(page));
-    },
+  loginPage: async ({ page }, use) => {
+    await use(new LoginPage(page));
+  },
+  inventoryPage: async ({ page }, use) => {
+    await use(new InventoryPage(page));
+  },
+  cartPage: async ({ page }, use) => {
+    await use(new CartPage(page));
+  },
+  checkoutPage: async ({ page }, use) => {
+    await use(new CheckoutPage(page));
+  },
+  headerComponent: async ({ page }, use) => {
+    await use(new HeaderComponent(page));
+  },
 });
 
-export {expect} from '@playwright/test';
+export { expect } from '@playwright/test';
