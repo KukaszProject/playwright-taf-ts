3a7237e293a51cef828092539e9c34bfa873047e
Lukasz Kielbasa <hamsingming@gmail.com>
2026-07-24 10:32:41 +0200

    Added waits for consistency before scanning

--- tests/ui/accessibility.spec.ts
+++ tests/ui/accessibility.spec.ts
@@ -2,41 +2,47 @@ import { test, expect } from '../../fixtures/base.test';
 import AxeBuilder from '@axe-core/playwright';
 
 test.describe('Accessibility Tests - logged out - @a11y', () => {
-    test.use({ storageState: {cookies: [], origins: []} });
+  test.use({ storageState: { cookies: [], origins: [] } });
 
-    test('Login Page Accessibility', async ({page}) => {
-        await page.goto('/');
-        const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
-        expect(accessibilityScanResults.violations).toEqual([]);
-    });
+  test('Login Page Accessibility', async ({ page, loginPage }) => {
+    await page.goto('/');
+    await loginPage.waitForReady();
 
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+    expect(accessibilityScanResults.violations).toEqual([]);
+  });
 });
 
 test.describe('Accessibility Tests - logged in - @a11y', () => {
-    test('Inventory Page Accessibility', async ({page}) => {
-        await page.goto('/inventory.html');
+  test('Inventory Page Accessibility', async ({ page, inventoryPage }) => {
+    await page.goto('/inventory.html');
+    await inventoryPage.waitForReady();
 
-        const accessibilityScanResults = await new AxeBuilder({ page }).exclude('.known-bad-components').analyze();
-        expect(accessibilityScanResults.violations).toEqual([]);
-    });
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+    expect(accessibilityScanResults.violations).toEqual([]);
+  });
 
-    test('Cart Page Accessibility', async ({page, inventoryPage}) => {
-        await page.goto('/inventory.html');
-        await inventoryPage.addItemsToCart(['Sauce Labs Backpack', 'Sauce Labs Bike Light']);
-        await inventoryPage.goToShoppingCart();
+  test('Cart Page Accessibility', async ({ page, inventoryPage, cartPage }) => {
+    await page.goto('/inventory.html');
+    await inventoryPage.waitForReady();
+    await inventoryPage.addItemsToCart(['Sauce Labs Backpack', 'Sauce Labs Bike Light']);
+    await inventoryPage.goToShoppingCart();
+    await cartPage.waitForReady();
 
-        const accessibilityScanResults = await new AxeBuilder({ page }).exclude('.known-bad-components').analyze();
-        expect(accessibilityScanResults.violations).toEqual([]);
-    });
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+    expect(accessibilityScanResults.violations).toEqual([]);
+  });
 
-    test('Checkout Page Accessibility', async ({page, inventoryPage, cartPage}) => {
-        await page.goto('/inventory.html');
-        await inventoryPage.addItemsToCart(['Sauce Labs Backpack', 'Sauce Labs Bike Light']);
-        await inventoryPage.goToShoppingCart();
-        await cartPage.goToCheckout();
-        
-        const accessibilityScanResults = await new AxeBuilder({ page }).exclude('.known-bad-components').analyze();
-        expect(accessibilityScanResults.violations).toEqual([]);
-    });
+  test('Checkout Page Accessibility', async ({ page, inventoryPage, cartPage, checkoutPage }) => {
+    await page.goto('/inventory.html');
+    await inventoryPage.waitForReady();
+    await inventoryPage.addItemsToCart(['Sauce Labs Backpack', 'Sauce Labs Bike Light']);
+    await inventoryPage.goToShoppingCart();
+    await cartPage.waitForReady();
+    await cartPage.goToCheckout();
+    await checkoutPage.waitForReady();
 
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+    expect(accessibilityScanResults.violations).toEqual([]);
+  });
 });
